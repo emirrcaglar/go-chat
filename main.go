c024379af893620f5cf7cd8f9a219a78a1a77e41
emirrcaglar <emirrcaglar@gmail.com>
2025-07-22 16:06:14 +0300

    no broadcast

--- main.go
+++ main.go
@@ -19,42 +19,37 @@ func NewServer() *Server {
 }
 
 func (s *Server) handleWS(ws *websocket.Conn) {
-	fmt.Println("new incoming connection from client: ", ws.RemoteAddr())
+	fmt.Printf("new connection: %v", ws.RemoteAddr())
 
 	s.conns[ws] = true
-
 	s.readLoop(ws)
 }
 
 func (s *Server) readLoop(ws *websocket.Conn) {
-	buf := make([]byte, 1024)
+	buf := make([]byte, 1024) // slice of bytes with size of 1024
+
 	for {
 		n, err := ws.Read(buf)
 		if err != nil {
 			if err == io.EOF {
 				break
 			}
-			fmt.Println("read error: ", err)
+			fmt.Printf("read error:%v", err)
 			continue
 		}
 		msg := buf[:n]
+		fmt.Println(string(msg))
 
-		s.broadcast(msg)
-	}
-}
-
-func (s *Server) broadcast(b []byte) {
-	for ws := range s.conns {
-		go func(ws *websocket.Conn) {
-			if _, err := ws.Write(b); err != nil {
-				fmt.Println("write error:", err)
-			}
-		}(ws)
+		ws.Write([]byte("thank you for the msg"))
 	}
 }
 
 func main() {
 	server := NewServer()
+
 	http.Handle("/ws", websocket.Handler(server.handleWS))
-	http.ListenAndServe(":3000", nil)
+	fmt.Println("Server starting on port 3000...")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Printf("Error starting server:%v", err)
+	}
 }
